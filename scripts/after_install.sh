0fae0abb15284633df3f1510590409497e20506e
Mijsters <yymijsters@deloitte.ch>
2022-07-12 18:07:44 +0200

    With Client BuildScript

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,2 +1,3 @@
 cd /home/ec2-user
 npm i
+npm run postbuild
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -9,13 +10,18 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 // Define routes
 app.use("/api/user", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/goal", require("./routes/api/goal"));
+
+//Serve static assets in production
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
