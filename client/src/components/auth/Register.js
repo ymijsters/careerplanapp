f3acb6891bbd44dfc8603d9da53f975f050df2ef
Mijsters <yymijsters@deloitte.ch>
2022-07-13 14:32:41 +0200

    updated logic for register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,8 +2,17 @@ import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Register = () => {
-  const [formData, setFormData] = useState();
-  
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign Up</h1>
@@ -12,21 +21,24 @@ export const Register = () => {
       </p>
       <form className='form' action='create-profile.html'>
         <div className='form-group'>
-          <input type='text' placeholder='Name' name='name' required />
-        </div>
-        <div className='form-group'>
-          <input type='email' placeholder='Email Address' name='email' />
-          <small className='form-text'>
-            This site uses Gravatar so if you want a profile image, use a
-            Gravatar email
-          </small>
+          <input
+            type='email'
+            placeholder='Email Address'
+            value={email}
+            onChange={(e) => onChange(e)}
+            name='email'
+            required
+          />
         </div>
         <div className='form-group'>
           <input
             type='password'
             placeholder='Password'
             name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
             minLength='6'
+            required
           />
         </div>
         <div className='form-group'>
@@ -34,7 +46,10 @@ export const Register = () => {
             type='password'
             placeholder='Confirm Password'
             name='password2'
+            value={password2}
+            onChange={(e) => onChange(e)}
             minLength='6'
+            required
           />
         </div>
         <input type='submit' className='btn btn-primary' value='Register' />
