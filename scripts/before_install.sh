a897e7fd6c5c404fb18de35baecb271f573aef43
Mijsters <yymijsters@deloitte.ch>
2022-07-13 13:47:05 +0200

    updated kill command

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 sudo rm -f /home/ec2-user/stdout.txt
 sudo rm -f /home/ec2-user/stderr.txt
-for p in `ps -e|grep "node"|cut -f1 -d' '`; do    sudo pkill -f $p; done
-for p in `ps -e|grep "npm"|cut -f1 -d' '`; do    sudo pkill -f $p; done
+for p in `ps -e|grep "node"|cut -f1 -d' '`; do   kill -9 $p  > /dev/null 2> /dev/null || :; done
+for p in `ps -e|grep "npm"|cut -f1 -d' '`; do    kill -9 $p  > /dev/null 2> /dev/null || :; done
