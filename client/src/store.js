75a68ac9dfd4e4f15d54d47c542401d510211fd6
Mijsters <yymijsters@deloitte.ch>
2022-07-13 17:28:17 +0200

    now also install client

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import alertReducer from "../src/reducers/alert";
 
 export const store = configureStore({
   reducer: {
     alert: alertReducer,
-    filters: filtersReducer,
   },
 });
