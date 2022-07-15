de0d4c88ca39fbd81fb3da25c907d669480916d8
Mijsters <yymijsters@deloitte.ch>
2022-07-15 10:15:23 +0200

    Another test

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
-import api from "../utils/api";
+//import axios from "axios";
+//import api from "../utils/api";
 //import { addAlertWithTimout } from "./alert";
 
 const authSlice = createSlice({
