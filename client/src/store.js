03b7fa84864f3af99e3e1174977b06951cad1524
Mijsters <yymijsters@deloitte.ch>
2022-07-15 13:23:27 +0200

    Added Token handling

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import alertReducer from "../src/reducers/alert";
 import authReducer from "../src/reducers/auth";
+import setAuthToken from './utils/setAuthToken';
 
 const store = configureStore({
   reducer: {
@@ -8,4 +9,22 @@ const store = configureStore({
     auth: authReducer,
   },
 });
+
+/*
+  initialize current state from redux store for subscription comparison
+  preventing undefined error
+ */
+let currentState = store.getState();
+
+store.subscribe(() => {
+  // keep track of the previous and current state to compare changes
+  let previousState = currentState;
+  currentState = store.getState();
+  // if the token changes set the value in localStorage and axios headers
+  if (previousState.auth.token !== currentState.auth.token) {
+    const token = currentState.auth.token;
+    setAuthToken(token);
+  }
+});
+
 export default store;
