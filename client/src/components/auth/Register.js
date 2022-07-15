1f53ffbfa0b2175bbfcb8966f5a7691665de3035
Mijsters <yymijsters@deloitte.ch>
2022-07-15 12:22:24 +0200

    Fixed error

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addAlertWithTimout } from "../../reducers/alert";
 import { addUpdateUser } from "../../reducers/auth";
