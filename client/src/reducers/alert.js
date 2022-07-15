e1ebabd90959b0fc43da7cc8d5a897ba17f7e07a
Mijsters <yymijsters@deloitte.ch>
2022-07-15 10:07:51 +0200

    Trying something else

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -24,8 +24,8 @@ const alertSlice = createSlice({
 export const { setAlert, removeAlert } = alertSlice.actions;
 export default alertSlice.reducer;
 
-export function addAlertWithTimout(dispatch, alert) {
+export const addAlertWithTimout = (alert) => (dispatch) => {
   const id = uuidv4();
   dispatch(setAlert({ ...alert, id: id }));
   setTimeout(() => dispatch(removeAlert(id)), 5000);
-}
+};
