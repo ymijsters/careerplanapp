61414bcf74a512ef0ce5f83474049691850df8e4
Mijsters <yymijsters@deloitte.ch>
2022-07-21 12:48:41 +0200

    Updated path to background img

--- client/src/components/auth/AuthAside.js
+++ client/src/components/auth/AuthAside.js
@@ -5,7 +5,7 @@ export const AuthAside = () => {
     <div
       class='d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2'
       style={{
-        backgroundImage: 'url("./img/auth-bg.png")',
+        backgroundImage: 'url("../../img/auth-bg.png")',
       }}
     >
       <div class='d-flex flex-column flex-center py-15 px-5 px-md-15 w-100'>
