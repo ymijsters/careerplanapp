6582b1dfa5e9b02110ab87974a10e605def3a2b2
Mijsters <yymijsters@deloitte.ch>
2022-07-22 23:38:23 +0200

    Added input fields

--- client/src/components/profile/PersonalDataStep.js
+++ client/src/components/profile/PersonalDataStep.js
@@ -1,6 +1,20 @@
 import React from "react";
 
 export const PersonalDataStep = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    dateOfBirth: "",
+    currentCompany: "",
+    unemployed: false,
+    currentFunction: "",
+  });
+
+  const { name, dateOfBirth, currentCompany, unemployed, currentFunction } =
+    formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
   return (
     <form
       className='my-auto pb-5'
@@ -12,7 +26,7 @@ export const PersonalDataStep = () => {
         <div className='w-100'>
           <div className='pb-10 pb-lg-15'>
             <h2 className='fw-bold d-flex align-items-center text-dark'>
-              Choose Account Type
+              Who are you?
               <i
                 className='fas fa-exclamation-circle ms-2 fs-7'
                 data-bs-toggle='tooltip'
@@ -20,12 +34,35 @@ export const PersonalDataStep = () => {
               ></i>
             </h2>
             <div className='text-muted fw-semibold fs-6'>
-              If you need more info, please check out
+              Before you dive in, please tell us a little about yourself. Review
+              the &nbsp;
               <a href='#' className='link-primary fw-bold'>
-                Help Page
+                Privacy Statement
               </a>
-              .
+              &nbsp;to see how we keep your data save.
             </div>
+          </div>
+
+          <div className='mb-10 fv-row'>
+            <label className='form-label mb-3'>Your Name</label>
+            <input
+              type='text'
+              className='form-control form-control-lg form-control-solid'
+              name='account_name'
+              placeholder=''
+              onChange={(e) => onChange(e)}
+              value={name}
+            />
+          </div>
+
+          <div className='mb-10 fv-row'>
+            <label className='form-label mb-3'>Date of Birth</label>
+            <input
+              className='form-control form-control-solid'
+              name='calendar_event_start_date'
+              placeholder='Pick a start date'
+              id='kt_calendar_datepicker_start_date'
+            />
           </div>
         </div>
       </div>
