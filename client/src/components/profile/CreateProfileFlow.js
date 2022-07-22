007ed127f95bdc95b73fcc61b35f7d1115c086e2
Mijsters <yymijsters@deloitte.ch>
2022-07-22 22:51:19 +0200

    Fix errors PersonalDataStep

--- client/src/components/profile/CreateProfileFlow.js
+++ client/src/components/profile/CreateProfileFlow.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProfileAside } from "./ProfileAside";
+import { PersonalDataStep } from "./PersonalDataStep";
 
 export const CreateProfileFlow = () => {
   const [step, setStep] = useState({
