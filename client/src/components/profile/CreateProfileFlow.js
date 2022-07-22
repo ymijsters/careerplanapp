64f947bd919c380fd11ef1c26416f828a62b25a0
Mijsters <yymijsters@deloitte.ch>
2022-07-22 23:20:59 +0200

    Changed Switch into function

--- client/src/components/profile/CreateProfileFlow.js
+++ client/src/components/profile/CreateProfileFlow.js
@@ -23,14 +23,14 @@ export const CreateProfileFlow = () => {
       <div className='d-flex flex-column flex-lg-row-fluid py-10'>
         <div className='d-flex flex-center flex-column flex-column-fluid'>
           <div className='w-lg-650px w-xl-700px p-10 p-lg-15 mx-auto'>
-            {() => {
+            {(() => {
               switch (step) {
                 case 1:
                   return <PersonalDataStep />;
                 default:
                   return <p>Default Return here</p>;
               }
-            }}
+            })()}
             <p onClick={() => nextStep()}>Click for next step</p>
           </div>
         </div>
