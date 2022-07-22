7594c189ce2a2e00e38102019d6f6f4615185140
Mijsters <yymijsters@deloitte.ch>
2022-07-23 00:12:51 +0200

    Fixed Previous button hopefully

--- client/src/components/profile/PersonalDataStep.js
+++ client/src/components/profile/PersonalDataStep.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export const PersonalDataStep = () => {
+export const PersonalDataStep = (nextStep) => {
   const [formData, setFormData] = useState({
     name: "",
     currentCompany: "",
@@ -17,6 +17,15 @@ export const PersonalDataStep = () => {
       setFormData({ ...formData, [e.target.name]: e.target.value });
     }
   };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await dispatch(login(email, password));
+      props.nextStep();
+    } catch {}
+  };
+
   return (
     <form
       className='my-auto pb-5'
@@ -97,7 +106,6 @@ export const PersonalDataStep = () => {
               <button
                 type='button'
                 className='btn btn-lg btn-light-primary me-3'
-                data-kt-stepper-action='previous'
               >
                 <span className='svg-icon svg-icon-4 me-1'>
                   <svg
@@ -126,48 +134,7 @@ export const PersonalDataStep = () => {
               </button>
             </div>
             <div>
-              <button
-                type='button'
-                className='btn btn-lg btn-primary'
-                data-kt-stepper-action='submit'
-              >
-                <span className='indicator-label'>
-                  Submit
-                  <span className='svg-icon svg-icon-4 ms-2'>
-                    <svg
-                      width='24'
-                      height='24'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <rect
-                        opacity='0.5'
-                        x='18'
-                        y='13'
-                        width='13'
-                        height='2'
-                        rx='1'
-                        transform='rotate(-180 18 13)'
-                        fill='currentColor'
-                      />
-                      <path
-                        d='M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z'
-                        fill='currentColor'
-                      />
-                    </svg>
-                  </span>
-                </span>
-                <span className='indicator-progress'>
-                  Please wait...
-                  <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
-                </span>
-              </button>
-              <button
-                type='button'
-                className='btn btn-lg btn-primary'
-                data-kt-stepper-action='next'
-              >
+              <button type='button' className='btn btn-lg btn-primary'>
                 Continue
                 <span className='svg-icon svg-icon-4 ms-1'>
                   <svg
