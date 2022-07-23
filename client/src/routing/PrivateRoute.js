cd339b6e1af406fa29a4ca81cb54c459b8781c14
Mijsters <yymijsters@deloitte.ch>
2022-07-23 11:32:54 +0200

    Removed spinner

--- client/src/routing/PrivateRoute.js
+++ client/src/routing/PrivateRoute.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Spinner } from "../layout/spinner";
+//import { Spinner } from "../layout/spinner";
 import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = (props) => {
   const auth = useSelector((state) => state.auth);
-  if (auth.loading) return <Spinner />;
+  // if (auth.loading) return <Spinner />;
   if (auth.isAuthenticated) return <props.component />;
 
   return <Navigate to='/login' />;
